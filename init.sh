6e626661b99c092871b109aed26d496db7a865d3
0xDkXy <bo001015@outlook.com>
2023-01-29 15:26:40 +0800

    chore: add submodule

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -e
+
 echo "copying dos config"
 cp -r ./dotfile/.dosbox ~/
+
+echo -e  "mount c: $(pwd)\nc:\nPATH C:\\masm;C:\\;\ncd src" >> ~/.dosbox/dosbox-0.74-3.conf
 echo "Done!"
 cd ..
-mv ./AssemblyLanguageEnv ./dos
 
 
